6fd8284c1e0374ccb162934ded7c84765a0bff0a
Sebastian Tkaczyk <sebastian.tkaczykk@gmail.com>
2022-05-15 20:44:07 +0200

    feat: Add empty Body component

--- src/components/admin/Home.vue
+++ src/components/admin/Home.vue
@@ -2,15 +2,17 @@
   <div class="flex-container full-height">
     <Navbar/>
     <MenuAdmin/>
+    <Body/>
   </div>
 </template>
 <script>
 import Navbar from "@/components/admin/Navbar";
 import MenuAdmin from "@/components/admin/MenuAdmin";
+import Body from "@/components/admin/Body";
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
-  components: {MenuAdmin, Navbar}
+  components: {Body, MenuAdmin, Navbar}
 };
 </script>
