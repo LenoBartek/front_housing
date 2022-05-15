7fdc191bca48569befae95b9d1b5629a1383ec18
Sebastian Tkaczyk <sebastian.tkaczykk@gmail.com>
2022-05-15 20:31:08 +0200

    feat: Add MenuAdmin as aside menu

--- src/components/admin/Home.vue
+++ src/components/admin/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="flex-container full-height">
     <Navbar/>
+    <MenuAdmin/>
   </div>
 </template>
 <script>
 import Navbar from "@/components/admin/Navbar";
+import MenuAdmin from "@/components/admin/MenuAdmin";
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
-  components: {Navbar}
+  components: {MenuAdmin, Navbar}
 };
 </script>
---
+++ src/components/admin/MenuAdmin.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="sidebar">
+    <ul>
+      <li><a class="active" href="#Nieruchomosci">Nieruchomosci</a></li>
+      <li><a href="#Liczniki">Liczniki</a></li>
+      <li><a href="#Next">Next</a></li>
+      <li><a href="#Next">Next</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'MenuAdmin',
+  components: {},
+}
+</script>
+<style scoped>
+
+.sidebar{
+  height: 80vh;
+  width: 15vw;
+  float: left;
+  font-size: 20px;
+  background-color: #f1f1f1;
+  box-sizing: border-box;
+  display: inline-block;
+}
+
+.sidebar ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.sidebar li a {
+  display: block;
+  color: #000;
+  padding: 15px;
+  border-bottom: 1px solid #bdb8d7;
+  border-bottom: 1px solid rgba(0,0,0,0.05);
+  border-top: 1px solid rgba(255,255,255,0.05);
+}
+
+.sidebar li a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+.sidebar li a:hover:not(.active) {
+  background-color: #555;
+  color: white;
+}
+</style>
