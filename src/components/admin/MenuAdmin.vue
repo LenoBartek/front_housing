63470b5e0685906ebe610be69019681f8c759e3c
Leno <72757737+LenoBartek@users.noreply.github.com>
2022-07-27 19:24:45 +0200

    fix: Mouse hover, routes

--- src/components/admin/MenuAdmin.vue
+++ src/components/admin/MenuAdmin.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="sidebar">
     <ul>
-      <li v-for="mem in members"  :key="mem.id">
-        <a :class="{ active: (mem.id === active)  }" v-on:click="selectMember(mem.id)">
-          {{ mem.category}}
+      <li v-for="mem in members" :key="mem.id">
+        <a
+          :class="{ active: mem.id === active }"
+          v-on:click="selectMember(mem.id)"
+        >
+          {{ mem.category }}
         </a>
       </li>
     </ul>
@@ -11,33 +14,32 @@
 </template>
 
 <script>
-import store from '../../store/index'
+import store from "../../store/index";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'MenuAdmin',
+  name: "MenuAdmin",
   data() {
     return {
       active: store.getters.activeTabId,
       members: [
-        {id: 1, category: 'Nieruchomosci', path: '/immovable'},
-        {id: 2, category: 'Mieszkańcy' , path: '/resident'},
-        {id: 3, category: 'Głosowania', path: '/immovable'},
-        {id: 4, category: 'Usterki', path: '/immovable'}
-      ]
-    }
+        { id: 1, category: "Nieruchomosci", path: "/immovable" },
+        { id: 2, category: "Mieszkańcy", path: "/resident" },
+        { id: 3, category: "Głosowania", path: "/vote" },
+        { id: 4, category: "Usterki", path: "/fault" },
+      ],
+    };
   },
   methods: {
     selectMember: function (id) {
-      store.commit('setActiveTabId',id);
+      store.commit("setActiveTabId", id);
       this.active = id;
-      this.$router.push(this.members[id-1].path);
-    }
-  }
-}
+      this.$router.push(this.members[id - 1].path);
+    },
+  },
+};
 </script>
 <style scoped>
-
-.sidebar{
+.sidebar {
   position: absolute;
   left: 0%;
   height: 80vh;
@@ -66,13 +68,15 @@ export default {
   color: #000;
   padding: 15px;
   border-bottom: 1px solid #bdb8d7;
-  border-bottom: 1px solid rgba(0,0,0,0.05);
-  border-top: 1px solid rgba(255,255,255,0.05);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.05);
+  border-top: 1px solid rgba(255, 255, 255, 0.05);
+  cursor: default;
+  text-decoration: none;
 }
 
 .active {
   color: white;
-  background-color: green;
+  background-color: #4f925b;
 }
 
 .sidebar li a:hover:not(.active) {
